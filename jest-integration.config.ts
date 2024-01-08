ef460550a01c8d57f3e81f9ade14a9612c7cdbeb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-01-08 15:52:10 -0300

    chore: add jest configs and scripts

---
+++ jest-integration.config.ts
@@ -0,0 +1,7 @@
+import config from './jest.config'
+
+config.testMatch = [
+  '**/*.test.ts'
+]
+
+export default config
---
+++ jest-unit.config.ts
@@ -0,0 +1,7 @@
+import config from './jest.config'
+
+config.testMatch = [
+  '**/*.spec.ts'
+]
+
+export default config
